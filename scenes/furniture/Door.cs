3b13a03144bab8805b7f664644fc48c2abcbd44e
ihnryna <151572051+ihnryna@users.noreply.github.com>
2026-04-15 05:03:01 +0300

    Door asset & doorframe logic

--- scenes/furniture/Door.cs
+++ scenes/furniture/Door.cs
@@ -6,6 +6,9 @@ public partial class Door : StaticBody2D
 	private AnimatedSprite2D sprite;
 	private CollisionShape2D collisionShape;
 	private InteractableComponent interactableComponent;
+	private StaticBody2D doorCollider;
+	private StaticBody2D doorframeCollider;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,6 +16,8 @@ public partial class Door : StaticBody2D
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
         interactableComponent = GetNode<InteractableComponent>("InteractableComponent");
+		doorCollider = GetNode<StaticBody2D>("DoorCollider");
+		doorframeCollider = GetNode<StaticBody2D>("DoorFrameCollider");
 
 		interactableComponent.InteractableActivated += OnInteractableActivated;
 		interactableComponent.InteractableDeactivated += OnInteractableDeactivated;
@@ -23,14 +28,14 @@ public partial class Door : StaticBody2D
     {
         sprite.Play("open_door");
         GD.Print("activated");
-		CollisionLayer = 2;
+		doorCollider.CollisionLayer = 2;
     }
 
     private void OnInteractableDeactivated()
     {
         sprite.Play("close_door");
         GD.Print("deactivated");
-		CollisionLayer = 1;
+		doorCollider.CollisionLayer = 1;
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
